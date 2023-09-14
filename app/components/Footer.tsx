945e6e739435b014d3c29298e53d5a8c5f44de29
nicolas <nicolas-dutertre@bbox.fr>
2023-09-14 10:41:47 +0200

    fix underline

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer: React.FC = () => {
     return (
       <footer className="bg-[#e94a34] text-white py-4 mt-12">
         <div className="container mx-auto text-center">
-          <p>&copy; {new Date().getFullYear()} <a href="https://normandiewebschool.fr" className="underline" target="_blank"> Normandie Web School. </a>Tous droits réservés.  Quizz App by <a href="https://portfolio.sutcorp-industry.com" className="underline" target="_blank">Sutsiki</a></p>
+          <p>&copy; {new Date().getFullYear()} <a href="https://normandiewebschool.fr" className="underline" target="_blank"> Normandie Web School.</a>Tous droits réservés.  Quizz App by <a href="https://portfolio.sutcorp-industry.com" className="underline" target="_blank">Sutsiki</a></p>
         </div>
       </footer>
     );
